1df5e4f449439f9dacf5ea250e9f12eb9cf2da73
thebasedtaka <darktaka9998@gmail.com>
2024-07-14 16:25:28 -0400

    Add project files.

---
+++ Aot/Program.cs
@@ -0,0 +1,6 @@
+﻿using System.Globalization; // To use CultureInfo.
+WriteLine("This is an ahead-of-time (AOT) compiled console app.");
+WriteLine("Current culture: {0}", CultureInfo.CurrentCulture.DisplayName);
+WriteLine("OS version: {0}", Environment.OSVersion);
+Write("Press any key to exit.");
+ReadKey(intercept: true); // Do not output the key that was pressed.
---
+++ AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Xml.Linq;
+
+XDocument doc = new();
+
+WriteLine($"Environment.Is64BitProcess = {Environment.Is64BitProcess}");
+WriteLine($"int.MaxValue = {int.MaxValue:N0}");
+WriteLine($"nint.MaxValue = {nint.MaxValue:N0}");
